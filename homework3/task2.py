0751500cd0aaa3b6cee82b7b0a7d9d188e3646e5
VidasBalasievichius <vidaslololo@gmail.com>
2024-07-17 21:16:33 +0300

    task2.py

--- homework3/task2.py
+++ homework3/task2.py
@@ -1,3 +1,10 @@
+with open('cats.txt', 'w', encoding='utf-8') as file:
+    file.write("60b90c1c13067a15887e1ae1,Tayson,3\n")
+    file.write("60b90c2413067a15887e1ae2,Vika,1\n")
+    file.write("60b90c2e13067a15887e1ae3,Barsik,2\n")
+    file.write("60b90c3b13067a15887e1ae4,Simon,12\n")
+    file.write("60b90c4613067a15887e1ae5,Tessi,5\n")
+
 def get_cats_info(path):
     cats_info = []
     
@@ -10,7 +17,7 @@ def get_cats_info(path):
                     cat_info = {
                         "id": parts[0],
                         "name": parts[1],
-                        "age": parts[2]
+                        "age": int(parts[2]) 
                     }
                     cats_info.append(cat_info)
                 else:
@@ -24,3 +31,6 @@ def get_cats_info(path):
     except Exception as e:
         print(f"Помилка при обробці файлу: {e}")
         return []
+
+cats_info = get_cats_info('cats.txt')
+print(cats_info)
